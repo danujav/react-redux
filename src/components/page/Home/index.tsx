7fed16a2f607beec4f038e8d28824f8ab113145a
danujav <danujagreru@gmail.com>
2024-06-21 14:10:36 +0530

    add and remove cart items successfully

--- src/components/page/Home/index.tsx
+++ src/components/page/Home/index.tsx
@@ -2,10 +2,19 @@ import productList from "../../../data/productList.json";
 import Button from "../../common/Button";
 import ProductDescription from "./ProductDescription";
 
+import cartSlice from "../../../data/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+
 const Home = () => {
+  const { addToCart, removeFromCart } = cartSlice.actions;
+  const dispatch = useDispatch();
+  const { cartProductIds } = useSelector((state: any) => state.cart);
+
+  console.log(cartProductIds);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
-      {productList.products.map((product) => (
+      {productList.products.map((product, index) => (
         <div
           key={product.id}
           className="card bg-slate-200 rounded-lg shadow-lg overflow-hidden hover:shadow-xl hover:bg-slate-300 transition-all duration-300"
@@ -27,10 +36,20 @@ const Home = () => {
               ${product.price}
             </div>
             <div className="flex items-center justify-center">
-              <Button
-                style="px-4 py-2 font-semibold text-white bg-blue-500 hover:bg-blue-700 rounded"
-                children="Add to cart"
-              />
+              {!cartProductIds.includes(product.id) && (
+                <Button
+                  style="px-4 py-2 font-semibold text-white bg-blue-500 hover:bg-blue-700 rounded"
+                  children="Add to cart"
+                  onClick={() => dispatch(addToCart(product.id))}
+                />
+              )}
+              {cartProductIds.includes(product.id) && (
+                <Button
+                  style="px-4 py-2 font-semibold text-white bg-blue-500 hover:bg-blue-700 rounded"
+                  children="Remove from cart"
+                  onClick={() => dispatch(removeFromCart(product.id))}
+                />
+              )}
             </div>
           </div>
         </div>
