45fb33c96eda3315fb7b6ee0b1086417d5c04e67
danujav <danujagreru@gmail.com>
2024-06-21 16:26:55 +0530

    cart products removed from the store & empty cart view done

--- src/components/page/Cart/index.tsx
+++ src/components/page/Cart/index.tsx
@@ -1,3 +1,66 @@
-export default function Cart () {
-    return <h1>Cart</h1>
+import productList from "../../../data/productList.json";
+import { useDispatch, useSelector } from "react-redux";
+import cartSlice from "../../../data/cartSlice";
+import EmptyCart from "../../../assets/EmptyCart.jpg";
+
+export default function Cart() {
+  const { cartProductIds } = useSelector((state: any) => state.cart);
+  const dispatch = useDispatch();
+  const { removeFromCart } = cartSlice.actions;
+
+  const cartProducts = productList.products.filter((product) =>
+    cartProductIds.includes(product.id)
+  );
+
+  return (
+    <div className="row gap-4 p-5">
+      <p className="text-3xl">Products in cart...</p>
+      {cartProducts.map((product) => (
+        <div
+          key={product.id}
+          className="card m-5 p-5 bg-slate-200 shadow-lg overflow-hidden hover:shadow-xl hover:bg-slate-300 transition-all duration-300"
+        >
+          <div className="grid grid-cols-2">
+            <div className="flex flex-row">
+              <img
+                src={product.image}
+                alt={product.name}
+                className="m-5 mt-8 w-40 h-28 object-cover"
+              />
+              <div className="pl-5 flex flex-col max-w-96 p-3">
+                <h2 className="text-xl font-semibold text-teal-950">
+                  {product.name}
+                </h2>
+                <div className="min-h-20">
+                  <p className="text-teal-900">{product.description}</p>
+                </div>
+
+                <div className="mt-4 text-lg font-bold text-red-950">
+                  ${product.price}
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row-reverse">
+              <i
+                onClick={() => dispatch(removeFromCart(product.id))}
+                className="hover:text-red-700 hover:cursor-pointer fa-regular fa-rectangle-xmark text-2xl text-black"
+              ></i>
+            </div>
+          </div>
+        </div>
+      ))}
+      {cartProducts.length == 0 && (
+        <div className="h-screen flex justify-center">
+          <div className="bg-slate-50 shadow-lg overflow-hidden flex flex-col items-center h-96 w-1/2">
+              <p className="m-5 text-3xl">Empty cart...</p>
+              <img
+                alt="empty photo"
+                src={EmptyCart}
+                className=" w-1/2 h-60 object-cover"
+              />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,35 @@
-/// <reference types="vite/client" />
+// src/custom.d.ts
+declare module "*.svg" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.jpg" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.jpeg" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.png" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.gif" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.bmp" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.tiff" {
+    const content: string;
+    export default content;
+}
